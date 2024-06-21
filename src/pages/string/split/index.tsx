4b8ba1f17cac9602edef5b2bc7602cc9a365faac
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2024-06-21 20:57:05 +0100

    chore: output selector

--- src/pages/string/split/index.tsx
+++ src/pages/string/split/index.tsx
@@ -1,19 +1,14 @@
-import ToolHeader from '../../../components/ToolHeader'
-import ToolLayout from '../../../components/ToolLayout'
-import { Box, Radio, Stack, TextField, useTheme } from '@mui/material'
-import Grid from '@mui/material/Grid'
-import Typography from '@mui/material/Typography'
-import Button from '@mui/material/Button'
-import PublishIcon from '@mui/icons-material/Publish'
-import ContentPasteIcon from '@mui/icons-material/ContentPaste'
-import DownloadIcon from '@mui/icons-material/Download'
-import React, { useEffect, useRef, useState } from 'react'
-import ToolTextInput from '../../../components/input/ToolTextInput'
-import ToolTextResult from '../../../components/result/ToolTextResult'
-import SettingsIcon from '@mui/icons-material/Settings'
-import { Field, Formik, FormikProps } from 'formik'
-import * as Yup from 'yup'
-import ToolOptions from '../../../components/ToolOptions'
+import ToolHeader from '../../../components/ToolHeader';
+import ToolLayout from '../../../components/ToolLayout';
+import { Box, Stack, TextField } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import React, { useEffect, useRef, useState } from 'react';
+import ToolTextInput from '../../../components/input/ToolTextInput';
+import ToolTextResult from '../../../components/result/ToolTextResult';
+import { Field, Formik, FormikProps } from 'formik';
+import * as Yup from 'yup';
+import ToolOptions from '../../../components/ToolOptions';
 
 type SplitOperatorType = 'symbol' | 'regex' | 'length' | 'chunks';
 const initialValues = {
@@ -21,45 +16,74 @@ const initialValues = {
   symbolValue: ' ',
   regexValue: '/\\s+/',
   lengthValue: '16',
-  chunksValue: '4'
-}
-const initialSplitOperators: {
+  chunksValue: '4',
+
+  outputSeparator: '\\n',
+  charBeforeChunk: '',
+  charAfterChunk: ''
+};
+const splitOperators: {
   title: string;
   description: string;
-  type: SplitOperatorType
-}[] = [{
-  title: 'Use a Symbol for Splitting', description: 'Character that will be used to\n' +
-    'break text into parts.\n' +
-    '(Space by default.)',
-  type: 'symbol'
-},
+  type: SplitOperatorType;
+}[] = [
+  {
+    title: 'Use a Symbol for Splitting',
+    description:
+      'Character that will be used to\n' +
+      'break text into parts.\n' +
+      '(Space by default.)',
+    type: 'symbol'
+  },
   {
     title: 'Use a Regex for Splitting',
     type: 'regex',
-    description: 'Regular expression that will be\n' +
+    description:
+      'Regular expression that will be\n' +
       'used to break text into parts.\n' +
       '(Multiple spaces by default.)'
   },
   {
-    title: 'Use Length for Splitting', description: 'Number of symbols that will be\n' +
-      'put in each output chunk.',
+    title: 'Use Length for Splitting',
+    description:
+      'Number of symbols that will be\n' + 'put in each output chunk.',
     type: 'length'
   },
   {
-    title: 'Use a Number of Chunks', description: 'Number of chunks of equal\n' +
-      'length in the output.',
+    title: 'Use a Number of Chunks',
+    description: 'Number of chunks of equal\n' + 'length in the output.',
     type: 'chunks'
-  }]
-
+  }
+];
+const outputOptions: {
+  description: string;
+  accessor: keyof typeof initialValues;
+}[] = [
+  {
+    description:
+      'Character that will be put\n' +
+      'between the split chunks.\n' +
+      '(It\'s newline "\\n" by default.)',
+    accessor: 'outputSeparator'
+  },
+  {
+    description: 'Character before each chunk',
+    accessor: 'charBeforeChunk'
+  },
+  {
+    description: 'Character after each chunk',
+    accessor: 'charAfterChunk'
+  }
+];
 const CustomRadioButton = ({
-                             fieldName,
-                             type,
-                             title,
-                             onTypeChange,
-                             value,
-                             description,
-                             onTextChange
-                           }: {
+  fieldName,
+  type,
+  title,
+  onTypeChange,
+  value,
+  description,
+  onTextChange
+}: {
   fieldName: string;
   title: string;
   type: SplitOperatorType;
@@ -68,42 +92,68 @@ const CustomRadioButton = ({
   description: string;
   onTextChange: (value: string) => void;
 }) => {
-  const onChange = () => onTypeChange(type)
-  return (<Box>
-      <Stack direction={'row'} sx={{ mt: 2, mb: 1, cursor: 'pointer' }} onClick={onChange} alignItems={'center'}
-             spacing={1}>
-        <Field
-          type="radio"
-          name={fieldName}
-          value={type}
-          onChange={onChange}
-        />
+  const onChange = () => onTypeChange(type);
+  return (
+    <Box>
+      <Stack
+        direction={'row'}
+        sx={{ mt: 2, mb: 1, cursor: 'pointer' }}
+        onClick={onChange}
+        alignItems={'center'}
+        spacing={1}
+      >
+        <Field type="radio" name={fieldName} value={type} onChange={onChange} />
         <Typography>{title}</Typography>
       </Stack>
-      <TextField sx={{ backgroundColor: 'white' }} value={value}
-                 onChange={event => onTextChange(event.target.value)} />
-      <Typography fontSize={12} mt={1}>{description}</Typography>
+      <InputWithDesc
+        value={value}
+        onChange={onTextChange}
+        description={description}
+      />
     </Box>
-  )
-}
+  );
+};
+
+const InputWithDesc = ({
+  description,
+  value,
+  onChange
+}: {
+  description: string;
+  value: string;
+  onChange: (value: string) => void;
+}) => {
+  return (
+    <Box>
+      <TextField
+        sx={{ backgroundColor: 'white' }}
+        value={value}
+        onChange={(event) => onChange(event.target.value)}
+      />
+      <Typography fontSize={12} mt={1}>
+        {description}
+      </Typography>
+    </Box>
+  );
+};
 export default function SplitText() {
-  const [input, setInput] = useState<string>('')
-  const [result, setResult] = useState<string>('')
-  const formRef = useRef<FormikProps<typeof initialValues>>()
+  const [input, setInput] = useState<string>('');
+  const [result, setResult] = useState<string>('');
+  const formRef = useRef<FormikProps<typeof initialValues>>();
   useEffect(() => {
-    setResult(input.split(' ').join('\n'))
-  }, [input])
+    setResult(input.split(' ').join('\n'));
+  }, [input]);
 
   const validationSchema = Yup.object({
-    splitSeparator: Yup.string().required('The separator is required')
-  })
+    // splitSeparator: Yup.string().required('The separator is required')
+  });
 
   return (
     <ToolLayout>
       <ToolHeader
         title={'Text Splitter'}
         description={
-          'World\'s simplest browser-based utility for splitting text. Load your text in the input form on the left and you\'ll automatically get pieces of this text on the right. Powerful, free, and fast. Load text – get chunks.'
+          "World's simplest browser-based utility for splitting text. Load your text in the input form on the left and you'll automatically get pieces of this text on the right. Powerful, free, and fast. Load text – get chunks."
         }
       />
 
@@ -120,29 +170,42 @@ export default function SplitText() {
           initialValues={initialValues}
           validationSchema={validationSchema}
           innerRef={formRef}
-          onSubmit={() => {
-          }}
+          onSubmit={() => {}}
         >
           {({ setFieldValue, values }) => (
-            <Stack direction={'row'}>
+            <Stack direction={'row'} spacing={2}>
               <Box>
                 <Typography fontSize={22}>Split separator options</Typography>
-                {initialSplitOperators.map(({ title, description, type }) =>
+                {splitOperators.map(({ title, description, type }) => (
                   <CustomRadioButton
+                    key={type}
                     type={type}
                     title={title}
                     fieldName={'splitSeparatorType'}
                     description={description}
                     value={values[`${type}Value`]}
-                    onTypeChange={(type) => setFieldValue('splitSeparatorType', type)}
+                    onTypeChange={(type) =>
+                      setFieldValue('splitSeparatorType', type)
+                    }
                     onTextChange={(val) => setFieldValue(`${type}Value`, val)}
-                  />)}
+                  />
+                ))}
               </Box>
-              <Box></Box>
+              <Box>
+                <Typography fontSize={22}>Output separator options</Typography>
+                {outputOptions.map((option) => (
+                  <InputWithDesc
+                    key={option.accessor}
+                    value={values[option.accessor]}
+                    onChange={(value) => setFieldValue(option.accessor, value)}
+                    description={option.description}
+                  />
+                ))}
+              </Box>
             </Stack>
           )}
         </Formik>
       </ToolOptions>
     </ToolLayout>
-  )
+  );
 }
