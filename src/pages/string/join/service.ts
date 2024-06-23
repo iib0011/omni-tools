42f4d5a52bcaaeba3784ccb7f65ffb6d33bc3b02
Made4Uo <made4uo.shop@gmail.com>
2024-06-23 15:44:34 -0700

    Fix checkboxes in join string

--- src/pages/string/join/service.ts
+++ src/pages/string/join/service.ts
@@ -5,13 +5,23 @@ export function mergeText(
   joinCharacter: string = ''
 ): string {
   let processedLines: string[] = text.split('\n');
+
   if (deleteTrailingSpaces) {
     processedLines = processedLines.map((line) => line.trimEnd());
   }
 
   if (deleteBlankLines) {
-    processedLines = processedLines.filter((line) => line.trim());
+    processedLines = processedLines.filter((line) => line.trim() !== '');
+  } else {
+    processedLines = processedLines.map((line) =>
+      line.trim() === '' ? line + '\r\n\n' : line
+    );
   }
-
   return processedLines.join(joinCharacter);
 }
+
+// Example text to use
+`This is a line with trailing spaces    
+Another line with trailing spaces   
+   
+Final line without trailing spaces`;
