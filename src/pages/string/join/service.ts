bb34fee40ccfe06829b9df058944d579ee2e0290
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2024-06-23 23:55:30 +0100

    Revert "Fix checkboxes in join string"

--- src/pages/string/join/service.ts
+++ src/pages/string/join/service.ts
@@ -5,23 +5,13 @@ export function mergeText(
   joinCharacter: string = ''
 ): string {
   let processedLines: string[] = text.split('\n');
-
   if (deleteTrailingSpaces) {
     processedLines = processedLines.map((line) => line.trimEnd());
   }
 
   if (deleteBlankLines) {
-    processedLines = processedLines.filter((line) => line.trim() !== '');
-  } else {
-    processedLines = processedLines.map((line) =>
-      line.trim() === '' ? line + '\r\n\n' : line
-    );
+    processedLines = processedLines.filter((line) => line.trim());
   }
+
   return processedLines.join(joinCharacter);
 }
-
-// Example text to use
-`This is a line with trailing spaces    
-Another line with trailing spaces   
-   
-Final line without trailing spaces`;
