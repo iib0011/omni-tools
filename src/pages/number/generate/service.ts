6cac75ee88b8b65b8986bb723221b90db0c9ce20
Chesterkxng <cgoita00@gmail.com>
2024-06-25 21:02:03 +0000

    writing standards fix

--- src/pages/number/generate/service.ts
+++ src/pages/number/generate/service.ts
@@ -4,9 +4,9 @@ export function listOfIntegers(
     step: number,
     separator: string) {
         const result: number[] = [];
-        for (let i: number= 0; i <number_of_numbers; i++) {
+        for (let i: number = 0; i < number_of_numbers; i++) {
             const value: number = first_value + i * step;
             result.push(value);
         }
         return result.join(separator);
-    }   
+    }  
