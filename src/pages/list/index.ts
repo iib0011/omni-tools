028270637026ec699ce88eda71b2599e6c0f2dd8
Chesterkxng <cgoita00@gmail.com>
2024-07-03 23:09:25 +0000

    rotate service and test cases; then updated index file

--- src/pages/list/index.ts
+++ src/pages/list/index.ts
@@ -1,3 +1,4 @@
+import { tool as listRotate } from './rotate/meta';
 import { tool as listTruncate } from './truncate/meta';
 import { tool as listShuffle } from './shuffle/meta';
 import { tool as listSort } from './sort/meta';
---
+++ src/pages/list/rotate/rotate.service.test.ts
@@ -0,0 +1,110 @@
+import { expect, describe, it } from 'vitest';
+import {
+    SplitOperatorType,
+    rotateList
+} from './service';
+
+describe('rotate function', () => {
+    it('should rotate right side if right is set to true', () => {
+        const input: string = 'apple, pineaple, lemon, orange, mango';
+        const splitOperatorType: SplitOperatorType = 'symbol';
+        const splitSeparator = ', ';
+        const joinSeparator = '  ';
+        const step = 1;
+        const right = true;
+
+        const result = rotateList(
+            splitOperatorType,
+            input,
+            splitSeparator,
+            joinSeparator,
+            right,
+
+            step);
+
+        expect(result).toBe('mango  apple  pineaple  lemon  orange');
+
+    });
+
+    it('should rotate left side if right is set to true', () => {
+        const input: string = 'apple, pineaple, lemon, orange, mango';
+        const splitOperatorType: SplitOperatorType = 'symbol';
+        const splitSeparator = ', ';
+        const joinSeparator = ' ';
+        const step = 1;
+        const right = false;
+
+        const result = rotateList(
+            splitOperatorType,
+            input,
+            splitSeparator,
+            joinSeparator,
+            right,
+
+            step);
+
+        expect(result).toBe('pineaple lemon orange mango apple');
+
+    });
+
+    it('should rotate left side with 2 step if right is set to true', () => {
+        const input: string = 'apple, pineaple, lemon, orange, mango';
+        const splitOperatorType: SplitOperatorType = 'symbol';
+        const splitSeparator = ', ';
+        const joinSeparator = ' ';
+        const step = 2;
+        const right = false;
+
+        const result = rotateList(
+            splitOperatorType,
+            input,
+            splitSeparator,
+            joinSeparator,
+            right,
+
+            step);
+
+        expect(result).toBe('lemon orange mango apple pineaple');
+
+    });
+
+    it('should raise an error if step is negative', () => {
+        const input: string = 'apple, pineaple, lemon, orange, mango';
+        const splitOperatorType: SplitOperatorType = 'symbol';
+        const splitSeparator = ', ';
+        const joinSeparator = ' ';
+        const step = -2;
+        const right = false;
+
+        expect(() => {
+            rotateList(
+                splitOperatorType,
+                input,
+                splitSeparator,
+                joinSeparator,
+                right,
+                step);
+        }).toThrowError('Rotation step must be greater than zero.');
+
+    });
+
+    it('should raise an error if step is undefined', () => {
+        const input: string = 'apple, pineaple, lemon, orange, mango';
+        const splitOperatorType: SplitOperatorType = 'symbol';
+        const splitSeparator = ', ';
+        const joinSeparator = ' ';
+        const right = false;
+
+        expect(() => {
+            rotateList(
+                splitOperatorType,
+                input,
+                splitSeparator,
+                joinSeparator,
+                right);
+        }).toThrowError('Rotation step contains non-digits.');
+
+    });
+
+
+})
