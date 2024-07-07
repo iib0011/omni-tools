67749431968402d7a70b730e04e6475e23e6a625
Chesterkxng <cgoita00@gmail.com>
2024-07-07 22:14:17 +0000

    reverse tool (easiest) and testCases then updated index file

--- src/pages/list/index.ts
+++ src/pages/list/index.ts
@@ -1,3 +1,4 @@
+import { tool as listReverse } from './reverse/meta';
 import { tool as listFindUnique } from './find-unique/meta';
 import { tool as listFindMostPopular } from './find-most-popular/meta';
 import { tool as listGroup } from './group/meta';
