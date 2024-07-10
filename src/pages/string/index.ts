2855efd8c73bbd5b75caf7404f016ac71ea3ee02
Chesterkxng <cgoita00@gmail.com>
2024-07-10 00:39:26 +0000

    palindrome tool, testCases then updated index file

--- src/pages/string/index.ts
+++ src/pages/string/index.ts
@@ -1,3 +1,4 @@
+import { tool as stringPalindrome } from './palindrome/meta';
 import { tool as stringToMorse } from './to-morse/meta';
 import { tool as stringSplit } from './split/meta';
 import { tool as stringJoin } from './join/meta';
