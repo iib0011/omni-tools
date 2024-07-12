626092508e4a26e1b2b9b79bb2e9bf2e5efe9564
Chesterkxng <cgoita00@gmail.com>
2024-07-12 20:30:43 +0000

    index file

--- src/pages/string/index.ts
+++ src/pages/string/index.ts
@@ -1,3 +1,4 @@
+import { tool as stringQuote } from './quote/meta';
 import { tool as stringRot13 } from './rot13/meta';
 import { tool as stringReverse } from './reverse/meta';
 import { tool as stringRandomizeCase } from './randomize-case/meta';
