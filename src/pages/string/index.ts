6b191eaa124dc1114b23861a249f33859c5e5be4
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2024-07-14 00:53:28 +0100

    chore: use string tools

--- src/pages/string/index.ts
+++ src/pages/string/index.ts
@@ -8,4 +8,14 @@ import { tool as stringToMorse } from './to-morse/meta';
 import { tool as stringSplit } from './split/meta';
 import { tool as stringJoin } from './join/meta';
 
-export const stringTools = [stringSplit, stringJoin, stringToMorse];
+export const stringTools = [
+  stringSplit,
+  stringJoin,
+  stringToMorse,
+  stringReverse,
+  stringRandomizeCase,
+  stringUppercase,
+  stringExtractSubstring,
+  stringCreatePalindrome,
+  stringPalindrome
+];
