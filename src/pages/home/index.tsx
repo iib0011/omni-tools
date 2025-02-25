54703b3d3ef6cf1ef1548b338293df19efb75fe1
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2025-02-25 13:55:35 +0000

    style: background svg

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,7 +5,13 @@ import Categories from './Categories';
 export default function Home() {
   return (
     <Box
-      padding={{ xs: 1, md: 3, lg: 5, backgroundColor: '#F5F5FA' }}
+      padding={{
+        xs: 1,
+        md: 3,
+        lg: 5,
+        background: 'url(src/assets/background.svg)',
+        backgroundColor: '#F5F5FA'
+      }}
       display={'flex'}
       flexDirection={'column'}
       alignItems={'center'}
