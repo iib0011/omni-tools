eb9e3cbdf9dd4ee24bba135858b240fe3ef28e50
EugSh <127679450+EugSh1@users.noreply.github.com>
2025-03-07 15:26:04 +0300

    feat: add text repeat tool
    - Added text repeat tool to repeat input text multiple times with optional delimiter.
    - Included functionality for custom repetition amounts and delimiters.

---
+++ src/pages/tools/string/repeat/meta.ts
@@ -0,0 +1,13 @@
+import { defineTool } from '@tools/defineTool';
+import { lazy } from 'react';
+
+export const tool = defineTool('string', {
+  name: 'Repeat text',
+  path: 'repeat',
+  shortDescription: 'Repeat text multiple times.',
+  icon: 'material-symbols-light:replay',
+  description:
+    'This tool allows you to repeat a given text multiple times with an optional separator.',
+  keywords: ['text', 'repeat'],
+  component: lazy(() => import('./index'))
+});
