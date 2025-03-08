a279c442ecc741873da8cf0a8799c8c36d26fd38
‘lukasHerajt’ <lherajt@gmail.com>
2025-03-08 07:46:41 -0500

    Uppercase Icon fixed

--- src/pages/tools/string/uppercase/meta.ts
+++ src/pages/tools/string/uppercase/meta.ts
@@ -4,7 +4,7 @@ import { lazy } from 'react';
 export const tool = defineTool('string', {
   name: 'Uppercase',
   path: 'uppercase',
-  icon: 'material-symbols-light:format-uppercase',
+  icon: 'material-symbols-light:text-fields',
   description:
     "World's simplest browser-based utility for converting text to uppercase. Just input your text and it will be automatically converted to all capital letters. Perfect for creating headlines, emphasizing text, or standardizing text format. Supports various text formats and preserves special characters.",
   shortDescription: 'Convert text to uppercase letters',
