fa0de1cf2d5fc9eaf4134dc9354449be604bc3e1
‘lukasHerajt’ <lherajt@gmail.com>
2025-03-08 07:46:31 -0500

    icons added to links

--- src/pages/tools/string/create-palindrome/meta.ts
+++ src/pages/tools/string/create-palindrome/meta.ts
@@ -5,9 +5,10 @@ import { lazy } from 'react';
 export const tool = defineTool('string', {
   name: 'Create palindrome',
   path: 'create-palindrome',
-  icon: '',
-  description: '',
-  shortDescription: '',
+  icon: 'material-symbols-light:repeat',
+  description:
+    "World's simplest browser-based utility for creating palindromes from any text. Input text and instantly transform it into a palindrome that reads the same forward and backward. Perfect for word games, creating symmetrical text patterns, or exploring linguistic curiosities.",
+  shortDescription: 'Create text that reads the same forward and backward',
   keywords: ['create', 'palindrome'],
   component: lazy(() => import('./index'))
 });
--- src/pages/tools/string/randomize-case/meta.ts
+++ src/pages/tools/string/randomize-case/meta.ts
@@ -5,9 +5,10 @@ import { lazy } from 'react';
 export const tool = defineTool('string', {
   name: 'Randomize case',
   path: 'randomize-case',
-  icon: '',
-  description: '',
-  shortDescription: '',
+  icon: 'material-symbols-light:format-textdirection-l-to-r',
+  description:
+    "World's simplest browser-based utility for randomizing the case of text. Just paste your text and get it instantly transformed with random uppercase and lowercase letters. Perfect for creating playful text styles, meme text, or simulating chaotic writing.",
+  shortDescription: 'Convert text to random uppercase and lowercase letters',
   keywords: ['randomize', 'case'],
   component: lazy(() => import('./index'))
 });
