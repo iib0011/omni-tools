15506c2841c1803100f33533934a6585bf76b025
Chesterkxng <cgoita00@gmail.com>
2025-03-26 21:35:21 +0000

    feat: truncate-clock-time

--- src/pages/tools/time/index.ts
+++ src/pages/tools/time/index.ts
@@ -2,10 +2,12 @@ import { tool as daysDoHours } from './convert-days-to-hours/meta';
 import { tool as hoursToDays } from './convert-hours-to-days/meta';
 import { tool as convertSecondsToTime } from './convert-seconds-to-time/meta';
 import { tool as convertTimetoSeconds } from './convert-time-to-seconds/meta';
+import { tool as truncateClockTime } from './truncate-clock-time/meta';
 
 export const timeTools = [
   daysDoHours,
   hoursToDays,
   convertSecondsToTime,
-  convertTimetoSeconds
+  convertTimetoSeconds,
+  truncateClockTime
 ];
