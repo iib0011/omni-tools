2280059e1488c374230eb486d67e680fa8ce4ec1
Chesterkxng <cgoita00@gmail.com>
2025-03-28 17:14:19 +0000

    feat: csv-rows-to-columns

--- src/pages/tools/csv/index.ts
+++ src/pages/tools/csv/index.ts
@@ -1,4 +1,5 @@
 import { tool as csvToJson } from './csv-to-json/meta';
 import { tool as csvToXml } from './csv-to-xml/meta';
+import { tool as csvToRowsColumns } from './csv-rows-to-columns/meta';
 
-export const csvTools = [csvToJson, csvToXml];
+export const csvTools = [csvToJson, csvToXml, csvToRowsColumns];
