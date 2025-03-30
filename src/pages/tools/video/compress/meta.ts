e53642db315d3b302593402cf7ed977f61b7cbf6
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2025-03-30 17:19:22 +0000

    chore: compress video icon

--- src/pages/tools/video/compress/meta.ts
+++ src/pages/tools/video/compress/meta.ts
@@ -4,7 +4,7 @@ import { lazy } from 'react';
 export const tool = defineTool('video', {
   name: 'Compress Video',
   path: 'compress',
-  icon: 'mdi:video-box',
+  icon: 'icon-park-outline:compression',
   description:
     'Compress videos by scaling them to different resolutions like 240p, 480p, 720p, etc. This tool helps reduce file size while maintaining acceptable quality. Supports common video formats like MP4, WebM, and OGG.',
   shortDescription: 'Compress videos by scaling to different resolutions',
