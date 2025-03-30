8b4384b499b374afacee42a96ac18e540eea4013
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2025-03-30 17:18:06 +0000

    feat: compress video

---
+++ src/pages/tools/video/compress/meta.ts
@@ -0,0 +1,20 @@
+import { defineTool } from '@tools/defineTool';
+import { lazy } from 'react';
+
+export const tool = defineTool('video', {
+  name: 'Compress Video',
+  path: 'compress',
+  icon: 'mdi:video-box',
+  description:
+    'Compress videos by scaling them to different resolutions like 240p, 480p, 720p, etc. This tool helps reduce file size while maintaining acceptable quality. Supports common video formats like MP4, WebM, and OGG.',
+  shortDescription: 'Compress videos by scaling to different resolutions',
+  keywords: [
+    'compress',
+    'video',
+    'resize',
+    'scale',
+    'resolution',
+    'reduce size'
+  ],
+  component: lazy(() => import('./index'))
+});
