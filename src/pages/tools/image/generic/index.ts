92858f2e241e7d26d69946b01451c8ee5dd939bb
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2025-04-02 19:53:33 +0000

    feat: svg change colors

--- src/pages/tools/image/generic/index.ts
+++ src/pages/tools/image/generic/index.ts
@@ -1,4 +1,5 @@
 import { tool as resizeImage } from './resize/meta';
 import { tool as compressImage } from './compress/meta';
+import { tool as changeColors } from './change-colors/meta';
 
-export const imageGenericTools = [resizeImage, compressImage];
+export const imageGenericTools = [resizeImage, compressImage, changeColors];
--- src/pages/tools/image/png/index.ts
+++ src/pages/tools/image/png/index.ts
@@ -2,14 +2,12 @@ import { tool as pngCrop } from './crop/meta';
 import { tool as pngCompressPng } from './compress-png/meta';
 import { tool as convertJgpToPng } from './convert-jgp-to-png/meta';
 import { tool as pngCreateTransparent } from './create-transparent/meta';
-import { tool as changeColorsInPng } from './change-colors-in-png/meta';
 import { tool as changeOpacity } from './change-opacity/meta';
 import { tool as removeBackground } from './remove-background/meta';
 
 export const pngTools = [
   pngCompressPng,
   pngCreateTransparent,
-  changeColorsInPng,
   convertJgpToPng,
   changeOpacity,
   pngCrop,
