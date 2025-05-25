d326d9acf72049e33b729369a362c473badcdc4a
nevolodia <bolodiab3007@gmail.com>
2025-05-25 16:20:38 +0200

    Link right click bug fixed.

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -100,12 +100,13 @@ const Navbar: React.FC<NavbarProps> = ({ onSwitchTheme }) => {
           alignItems: 'center'
         }}
       >
-        <img
-          onClick={() => navigate('/')}
-          style={{ cursor: 'pointer' }}
-          src={logo}
-          width={isMobile ? '80px' : '150px'}
-        />
+        <a href="/">
+          <img
+            src={logo}
+            width={isMobile ? '80px' : '150px'}
+            style={{ pointerEvents: 'none' }}
+          />
+        </a>
         {isMobile ? (
           <>
             <IconButton
