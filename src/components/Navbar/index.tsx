19c9ad717396f0ae4dd30ad3d824ae266163c1f1
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2025-05-26 19:49:50 +0100

    chore: remove unnecessary pointerEvents

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -101,11 +101,7 @@ const Navbar: React.FC<NavbarProps> = ({ onSwitchTheme }) => {
         }}
       >
         <a href="/">
-          <img
-            src={logo}
-            width={isMobile ? '80px' : '150px'}
-            style={{ pointerEvents: 'none' }}
-          />
+          <img src={logo} width={isMobile ? '80px' : '150px'} />
         </a>
         {isMobile ? (
           <>
