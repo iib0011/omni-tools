1dd434c9d189f46283b410458fa06b908023181e
Chesterkxng <cgoita00@gmail.com>
2025-05-26 15:37:31 +0200

    feat: temperature converter

--- src/pages/tools/number/generic-calc/data/index.ts
+++ src/pages/tools/number/generic-calc/data/index.ts
@@ -3,10 +3,12 @@ import voltageDropInWire from './voltageDropInWire';
 import sphereArea from './sphereArea';
 import sphereVolume from './sphereVolume';
 import slackline from './slackline';
+import temperatureConversion from './temperature';
 export default [
   ohmslaw,
   voltageDropInWire,
   sphereArea,
   sphereVolume,
-  slackline
+  slackline,
+  temperatureConversion
 ];
