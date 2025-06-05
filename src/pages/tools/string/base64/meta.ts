de47536f6617024cac6972a78015e52d16d28571
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2025-06-05 18:44:19 +0100

    fix: icon

--- src/pages/tools/string/base64/meta.ts
+++ src/pages/tools/string/base64/meta.ts
@@ -4,7 +4,7 @@ import { lazy } from 'react';
 export const tool = defineTool('string', {
   name: 'Base64',
   path: 'base64',
-  icon: 'mdi:code-tags',
+  icon: 'tabler:number-64-small',
   description:
     'A simple tool to encode or decode data using Base64, which is commonly used in web applications.',
   shortDescription: 'Encode or decode data using Base64.',
