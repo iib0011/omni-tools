82df4ee475f331097c0dc516495d8ae0fe817a25
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2025-07-09 02:16:29 +0100

    fix: remove xml viewer

--- src/pages/tools/xml/index.ts
+++ src/pages/tools/xml/index.ts
@@ -1,4 +1,4 @@
 import { tool as xmlXmlValidator } from './xml-validator/meta';
 import { tool as xmlXmlBeautifier } from './xml-beautifier/meta';
-import { tool as xmlXmlViewer } from './xml-viewer/meta';
-export const xmlTools = [xmlXmlViewer, xmlXmlBeautifier, xmlXmlValidator];
+
+export const xmlTools = [xmlXmlBeautifier, xmlXmlValidator];
--- src/pages/tools/xml/xml-viewer/types.ts
+++
@@ -1,3 +0,0 @@
-export type InitialValuesType = {
-  // splitSeparator: string;
-};
