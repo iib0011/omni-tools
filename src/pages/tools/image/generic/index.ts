1250c8f4c3d755a73e2969607ed37f8ebbb4efb8
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2025-07-09 02:55:24 +0100

    feat: convert to jpg

--- src/pages/tools/image/generic/index.ts
+++ src/pages/tools/image/generic/index.ts
@@ -8,6 +8,7 @@ import { tool as createTransparent } from './create-transparent/meta';
 import { tool as imageToText } from './image-to-text/meta';
 import { tool as qrCodeGenerator } from './qr-code/meta';
 import { tool as rotateImage } from './rotate/meta';
+import { tool as convertToJpg } from './convert-to-jpg/meta';
 export const imageGenericTools = [
   resizeImage,
   compressImage,
@@ -18,5 +19,6 @@ export const imageGenericTools = [
   createTransparent,
   imageToText,
   qrCodeGenerator,
-  rotateImage
+  rotateImage,
+  convertToJpg
 ];
