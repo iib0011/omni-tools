ecf62b0059ff9d4255f4986a39b7f6962de40741
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2025-07-18 02:42:11 +0100

    chore: translate merge videos

--- src/pages/tools/video/merge-video/meta.ts
+++ src/pages/tools/video/merge-video/meta.ts
@@ -2,13 +2,14 @@ import { defineTool } from '@tools/defineTool';
 import { lazy } from 'react';
 
 export const tool = defineTool('video', {
-  name: 'Merge Videos',
   path: 'merge-video',
   icon: 'fluent:merge-20-regular',
-  description: 'Combine multiple video files into one continuous video.',
-  shortDescription: 'Append and merge videos easily.',
   keywords: ['merge', 'video', 'append', 'combine'],
-  longDescription:
-    'This tool allows you to merge or append multiple video files into a single continuous video. Simply upload your video files, arrange them in the desired order, and merge them into one file for easy sharing or editing.',
-  component: lazy(() => import('./index'))
+  component: lazy(() => import('./index')),
+  i18n: {
+    name: 'video:mergeVideo.title',
+    description: 'video:mergeVideo.description',
+    shortDescription: 'video:mergeVideo.shortDescription',
+    longDescription: 'video:mergeVideo.longDescription'
+  }
 });
