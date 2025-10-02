28f4c64d3044df927dc088435164e803e14f8794
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2025-10-02 22:18:34 +0100

    feat: remove temperature conversion from generic-calc
    This commit removes the temperature conversion tool from the
    generic-calc tool. This is because the tool was causing issues.
    
    The following files were modified:
    - src/pages/tools/number/generic-calc/data/index.ts
    - src/pages/tools/number/generic-calc/data/temperature.ts
    - package.json
    - .idea/workspace.xml

--- src/pages/tools/number/generic-calc/data/index.ts
+++ src/pages/tools/number/generic-calc/data/index.ts
@@ -3,13 +3,11 @@ import voltageDropInWire from './voltageDropInWire';
 import sphereArea from './sphereArea';
 import sphereVolume from './sphereVolume';
 import slackline from './slackline';
-import temperatureConversion from './temperature';
 
 export default [
   ohmslaw,
   voltageDropInWire,
   sphereArea,
   sphereVolume,
-  slackline,
-  temperatureConversion
+  slackline
 ];
