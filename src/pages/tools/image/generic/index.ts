4396b709c9115b6ec4e99ec3887424965cfec672
meezumi <aaryank3000@gmail.com>
2025-11-25 17:07:28 +0530

    feat: Add Meme Generator tool with text customization options - fixes #271

--- src/pages/tools/image/generic/index.ts
+++ src/pages/tools/image/generic/index.ts
@@ -10,6 +10,7 @@ import { tool as qrCodeGenerator } from './qr-code/meta';
 import { tool as rotateImage } from './rotate/meta';
 import { tool as convertToJpg } from './convert-to-jpg/meta';
 import { tool as imageEditor } from './editor/meta';
+import { tool as memeGenerator } from './meme-generator/meta';
 export const imageGenericTools = [
   imageEditor,
   resizeImage,
@@ -22,5 +23,6 @@ export const imageGenericTools = [
   imageToText,
   qrCodeGenerator,
   rotateImage,
-  convertToJpg
+  convertToJpg,
+  memeGenerator
 ];
