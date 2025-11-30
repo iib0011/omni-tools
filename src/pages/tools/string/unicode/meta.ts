6ad57674ea512b69a7c27914a30bf626b258c243
Chesterkxng <cgoita00@gmail.com>
2025-11-30 17:57:48 +0100

    chore: keywords added to list

--- src/pages/tools/string/unicode/meta.ts
+++ src/pages/tools/string/unicode/meta.ts
@@ -5,11 +5,10 @@ export const tool = defineTool('string', {
   i18n: {
     name: 'string:unicode.title',
     description: 'string:unicode.description',
-    shortDescription: 'string:unicode.shortDescription',
-    longDescription: 'string:unicode.longDescription'
+    shortDescription: 'string:unicode.shortDescription'
   },
   path: 'unicode',
   icon: 'mdi:unicode',
-  keywords: ['unicode'],
+  keywords: ['unicode', 'encode', 'decode', 'escape', 'text'],
   component: lazy(() => import('./index'))
 });
