cdb0f5143b48260025834becbacec2ebdc196ef3
wz <wenzhe1423@gmail.com>
2026-04-10 10:32:48 +0800

    feat: convert minutes (every and every n minute)

--- src/pages/tools/time/crontab-guru/service.ts
+++ src/pages/tools/time/crontab-guru/service.ts
@@ -1,6 +1,7 @@
 import cronstrue from 'cronstrue';
 import { isValidCron } from 'cron-validator';
 
+// Cron to Description
 export function explainCrontab(expr: string): string {
   try {
     return cronstrue.toString(expr);
@@ -19,4 +20,38 @@ export function main(input: string, _options: any): string {
     return 'Invalid crontab expression.';
   }
   return explainCrontab(input);
+}
+
+// Description to Cron
+// Minutes
+function convertMinutes(minutes: string) {
+  minutes = minutes.trim().toLowerCase();
+
+  // Split Based Off Spaces
+  const input = minutes.split(' ');
+
+  // If Left Blank Assume It Means Every
+  if (!minutes) return '*';
+
+  try {
+    // Every Minute
+    if (input[0] === 'every' && input.length === 1) {
+      return '*';
+    }
+
+    // Every N Minutes
+    if (input[0] === 'every' && input.length === 2) {
+      const value = Number(input[1]);
+      if (59 >= value && value >= 0) {
+        return `*/${value}`;
+      }
+
+      // Invalid Input
+      return '';
+    }
+
+    // Minute to Minute
+  } catch (error) {
+    return '';
+  }
 }
