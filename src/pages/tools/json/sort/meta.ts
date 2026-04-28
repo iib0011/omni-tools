d909186e6d1574fc1e187ff23db2936d15a23df4
Chesterkxng <cgoita00@gmail.com>
2026-04-28 03:18:04 +0200

    chore: updated meta

--- src/pages/tools/json/sort/meta.ts
+++ src/pages/tools/json/sort/meta.ts
@@ -2,13 +2,14 @@ import { defineTool } from '@tools/defineTool';
 import { lazy } from 'react';
 
 export const tool = defineTool('json', {
-  path: 'sort-json',
-  icon: 'material-symbols:sort',
+  path: 'sort',
+  icon: 'hugeicons:sorting-05',
   keywords: ['json', 'sort', 'order', 'array', 'key'],
   component: lazy(() => import('./index')),
   i18n: {
     name: 'json:sortJson.title',
     description: 'json:sortJson.description',
-    shortDescription: 'json:sortJson.shortDescription'
+    shortDescription: 'json:sortJson.shortDescription',
+    longDescription: 'json:sortJson.longDescription'
   }
 });
