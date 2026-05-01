654efb22d31b67e1cd9bf88c568a840d4f638893
Chesterkxng <cgoita00@gmail.com>
2026-05-01 02:51:18 +0200

    chore: less restrictive PdfImageObject type

--- src/pages/tools/pdf/extract-images-from-pdf/types.ts
+++ src/pages/tools/pdf/extract-images-from-pdf/types.ts
@@ -1,12 +1,5 @@
-// PDF Worker Extracts This Object From Images in Files
-export interface PdfImageObject {
-  width: number;
-  height: number;
-  bitmap: ImageBitmap;
-  ref: string;
-}
+export type PdfImageObject = any;
 
-// Used For Easier Iterations Prior to Drawing on Canvas
 export interface PreProcessImageObject {
   img: PdfImageObject;
   pageNum: number;
