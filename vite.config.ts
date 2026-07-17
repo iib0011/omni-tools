aee9ec4d5d86249310af8c96c0a613c4e16dcbfe
Chesterkxng <cgoita00@gmail.com>
2026-07-17 12:46:53 +0200

    chore: last commit hash added to env var

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,17 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import packageJson from './package.json';
+import { execSync } from 'node:child_process';
+
+const commitHash = execSync('git rev-parse --short HEAD').toString().trim();
 
 // https://vitejs.dev/config https://vitest.dev/config
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   define: {
     'process.env': {},
-    __APP_VERSION__: JSON.stringify(packageJson.version)
+    __APP_VERSION__: JSON.stringify(packageJson.version),
+    __COMMIT_HASH__: JSON.stringify(commitHash)
   },
   optimizeDeps: {
     exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util']
