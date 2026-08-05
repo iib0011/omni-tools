3c0566b06832e77b4c5d93cb9ee39bcdce8640af
Chesterkxng <cgoita00@gmail.com>
2026-08-05 12:33:04 +0200

    chore: crop video updated

--- src/pages/tools/video/crop-video/service.ts
+++ src/pages/tools/video/crop-video/service.ts
@@ -1,8 +1,6 @@
-import { FFmpeg } from '@ffmpeg/ffmpeg';
+import { runFFmpegTask } from 'lib/ffmpeg';
 import { fetchFile } from '@ffmpeg/util';
 import { InitialValuesType } from './types';
-
-const ffmpeg = new FFmpeg();
 
 export async function getVideoDimensions(
   file: File
@@ -32,36 +30,37 @@ export async function cropVideo(
   input: File,
   options: InitialValuesType
 ): Promise<File> {
-  if (!ffmpeg.loaded) {
-    await ffmpeg.load({
-      wasmURL:
-        'https://cdn.jsdelivr.net/npm/@ffmpeg/core@0.12.9/dist/esm/ffmpeg-core.wasm'
+  return runFFmpegTask(async ({ ffmpeg, tempFile }) => {
+    const inputName = tempFile('.mp4');
+    const outputName = tempFile('.mp4');
+
+    await ffmpeg.writeFile(inputName, await fetchFile(input));
+
+    const args = [];
+
+    if (options.width <= 0 || options.height <= 0) {
+      throw new Error('Width and height must be positive');
+    }
+
+    args.push('-i', inputName);
+    args.push(
+      '-vf',
+      `crop=${options.width}:${options.height}:${options.x}:${options.y}`
+    );
+    args.push('-c:v', 'libx264', '-preset', 'ultrafast', outputName);
+
+    await ffmpeg.exec(args);
+
+    const croppedData = await ffmpeg.readFile(outputName);
+
+    const blob = new Blob([new Uint8Array(croppedData as Uint8Array)], {
+      type: 'video/mp4'
     });
-  }
 
-  const inputName = 'input.mp4';
-  const outputName = 'output.mp4';
-  await ffmpeg.writeFile(inputName, await fetchFile(input));
-
-  const args = [];
-
-  if (options.width <= 0 || options.height <= 0) {
-    throw new Error('Width and height must be positive');
-  }
-
-  args.push('-i', inputName);
-  args.push(
-    '-vf',
-    `crop=${options.width}:${options.height}:${options.x}:${options.y}`
-  );
-  args.push('-c:v', 'libx264', '-preset', 'ultrafast', outputName);
-
-  await ffmpeg.exec(args);
-
-  const croppedData = await ffmpeg.readFile(outputName);
-  return await new File(
-    [new Blob([croppedData as any], { type: 'video/mp4' })],
-    `${input.name.replace(/\.[^/.]+$/, '')}_cropped.mp4`,
-    { type: 'video/mp4' }
-  );
+    return await new File(
+      [blob],
+      `${input.name.replace(/\.[^/.]+$/, '')}_cropped.mp4`,
+      { type: 'video/mp4' }
+    );
+  });
 }
