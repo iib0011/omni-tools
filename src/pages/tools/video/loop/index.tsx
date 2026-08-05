d10aafc19044948a8c2bc99f11996623ef69b367
Chesterkxng <cgoita00@gmail.com>
2026-08-05 13:42:26 +0200

    chore: loop video updated

--- src/pages/tools/video/loop/index.tsx
+++ src/pages/tools/video/loop/index.tsx
@@ -1,9 +1,8 @@
 import { Box } from '@mui/material';
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import ToolContent from '@components/ToolContent';
 import { ToolComponentProps } from '@tools/defineTool';
 import { GetGroupsType } from '@components/options/ToolOptions';
-import { CardExampleType } from '@components/examples/ToolExamples';
 import { loopVideo } from './service';
 import { InitialValuesType } from './types';
 import ToolVideoInput from '@components/input/ToolVideoInput';
@@ -12,6 +11,7 @@ import TextFieldWithDesc from '@components/options/TextFieldWithDesc';
 import { updateNumberField } from '@utils/string';
 import * as Yup from 'yup';
 import { useTranslation } from 'react-i18next';
+import { debounce } from 'lodash';
 
 const initialValues: InitialValuesType = {
   loops: 2
@@ -21,7 +21,7 @@ const validationSchema = Yup.object({
   loops: Yup.number().min(1, 'Number of loops must be greater than 1')
 });
 
-export default function Loop({ title, longDescription }: ToolComponentProps) {
+export default function Loop({ title }: ToolComponentProps) {
   const { t } = useTranslation('video');
   const [input, setInput] = useState<File | null>(null);
   const [result, setResult] = useState<File | null>(null);
@@ -32,13 +32,15 @@ export default function Loop({ title, longDescription }: ToolComponentProps) {
     try {
       setLoading(true);
       const resultFile = await loopVideo(input, values);
-      await setResult(resultFile);
+      setResult(resultFile);
     } catch (error) {
       console.error(error);
     } finally {
       setLoading(false);
     }
   };
+
+  const debouncedCompute = useCallback(debounce(compute, 1000), []);
 
   const getGroups: GetGroupsType<InitialValuesType> | null = ({
     values,
@@ -50,10 +52,18 @@ export default function Loop({ title, longDescription }: ToolComponentProps) {
         <Box>
           <TextFieldWithDesc
             onOwnChange={(value) =>
-              updateNumberField(value, 'loops', updateField)
+              updateNumberField(
+                String(Math.max(1, Number(value) || 1)),
+                'loops',
+                updateField
+              )
             }
             value={values.loops}
             label={t('loop.numberOfLoops')}
+            type="number"
+            inputProps={{
+              min: 1
+            }}
           />
         </Box>
       )
@@ -65,30 +75,18 @@ export default function Loop({ title, longDescription }: ToolComponentProps) {
       input={input}
       inputComponent={<ToolVideoInput value={input} onChange={setInput} />}
       resultComponent={
-        loading ? (
-          <ToolFileResult
-            value={null}
-            title={t('loop.loopingVideo')}
-            loading={true}
-            extension={''}
-          />
-        ) : (
-          <ToolFileResult
-            value={result}
-            title={t('loop.resultTitle')}
-            extension={'mp4'}
-          />
-        )
+        <ToolFileResult
+          value={result}
+          title={t('loop.resultTitle')}
+          loading={loading}
+          extension={'mp4'}
+        />
       }
       initialValues={initialValues}
       validationSchema={validationSchema}
       getGroups={getGroups}
       setInput={setInput}
-      compute={compute}
-      toolInfo={{
-        title: t('loop.toolInfo.title', { title }),
-        description: longDescription
-      }}
+      compute={debouncedCompute}
     />
   );
 }
--- src/pages/tools/video/loop/service.ts
+++ src/pages/tools/video/loop/service.ts
@@ -1,41 +1,38 @@
 import { InitialValuesType } from './types';
-import { FFmpeg } from '@ffmpeg/ffmpeg';
+import { runFFmpegTask } from 'lib/ffmpeg';
 import { fetchFile } from '@ffmpeg/util';
-
-const ffmpeg = new FFmpeg();
 
 export async function loopVideo(
   input: File,
   options: InitialValuesType
 ): Promise<File> {
-  if (!ffmpeg.loaded) {
-    await ffmpeg.load({
-      wasmURL:
-        'https://cdn.jsdelivr.net/npm/@ffmpeg/core@0.12.9/dist/esm/ffmpeg-core.wasm'
+  return runFFmpegTask(async ({ ffmpeg, tempFile }) => {
+    const inputName = tempFile('.mp4');
+    const outputName = tempFile('.mp4');
+    await ffmpeg.writeFile(inputName, await fetchFile(input));
+
+    const args = [];
+    const loopCount = options.loops - 1;
+
+    if (loopCount <= 0) {
+      return input;
+    }
+
+    args.push('-stream_loop', loopCount.toString());
+    args.push('-i', inputName);
+    args.push('-c:v', 'libx264', '-preset', 'ultrafast', outputName);
+
+    await ffmpeg.exec(args);
+
+    const loopedData = await ffmpeg.readFile(outputName);
+
+    const blob = new Blob([new Uint8Array(loopedData as Uint8Array)], {
+      type: 'video/mp4'
     });
-  }
-
-  const inputName = 'input.mp4';
-  const outputName = 'output.mp4';
-  await ffmpeg.writeFile(inputName, await fetchFile(input));
-
-  const args = [];
-  const loopCount = options.loops - 1;
-
-  if (loopCount <= 0) {
-    return input;
-  }
-
-  args.push('-stream_loop', loopCount.toString());
-  args.push('-i', inputName);
-  args.push('-c:v', 'libx264', '-preset', 'ultrafast', outputName);
-
-  await ffmpeg.exec(args);
-
-  const loopedData = await ffmpeg.readFile(outputName);
-  return await new File(
-    [new Blob([loopedData as any], { type: 'video/mp4' })],
-    `${input.name.replace(/\.[^/.]+$/, '')}_looped.mp4`,
-    { type: 'video/mp4' }
-  );
+    return await new File(
+      [blob],
+      `${input.name.replace(/\.[^/.]+$/, '')}_looped.mp4`,
+      { type: 'video/mp4' }
+    );
+  });
 }
